853012b296fd0b031d78d451f8c606b8727a9433
Arpan Dutta <smalldutta@gmail.com>
2021-04-19 11:54:51 +0300

    builds initial app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
+
+
+
+
+
+
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build (BuildContext context){
+    return new MaterialApp(
+      title: "Calcon",
+      theme: new ThemeData(
+          primarySwatch: Colors.red,
+          accentColor: Colors.deepOrange,
+          brightness: Brightness.dark
+      ),
+      home: new HomePage(),
+    );
+  }
+}
